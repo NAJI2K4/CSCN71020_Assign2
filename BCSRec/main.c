bdddb00dff0742a7dcc3cf03925e565f37c9b901
NAJI2K4 <145725032+NAJI2K4@users.noreply.github.com>
2023-10-13 09:05:46 -0400

    corrected getPerimeter by adding + *width

--- BCSRec/main.c
+++ BCSRec/main.c
@@ -85,7 +85,7 @@ void setWidth(int input, int *width) {
 }
 
 int getPerimeter(int *length, int *width) {
-	int perimeter = *length + *length + *width;
+	int perimeter = *length + *length + *width + *width;
 	return perimeter;
 }
 
